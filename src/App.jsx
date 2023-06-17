67517cc111368c8cac83b9f96573d2d5dd2cba97
Mark Prodehl <markprodehl@gmail.com>
2023-06-16 19:02:35 -0700

    Updated the app.js imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter } from "react-router-dom";
-import { About, Contact, Experience, Feedback, Hero, Navbar, Tech, Works, StarsCanvas } from './components'
+import { About, Hero, Navbar, StarsCanvas } from './components'
 
 
 const App = () => {
